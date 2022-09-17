a9592679c4051cefb4e84948d02a21002e8feb9d
gaganpreet-webdeveloper <gagandhani179@gmail.com>
2022-09-17 18:22:41 +0530

    done

--- README.md
+++ README.md
@@ -6,6 +6,17 @@
 
 #We have two keywords for loading the js file.
 
-1. async ---> loads the javascript parallerly. i.e when js tag occurs
-2. defer ---> load the javascript after the full html file load.
+- async ---> loads the javascript parallerly. i.e when js tag occurs
+- defer ---> load the javascript after the full html file load.
+```
+
+```bash
+# We have three types of Pop ups i.e
+
+- alert ----> Shows an alert message.
+- prompt ---> It takes a value from the user , using prompt box.
+- confirm ---> It is a confirm box.
+
+- prompt takes two values i.e your question , default value .....like
+    - prompt('Enter your age ? ', 20);
 ```
--- js/first.js
+++ js/first.js
@@ -1,6 +1,25 @@
+/*
 let x = document.getElementById("h");
 x.addEventListener("click", function click(e) {
-	e.target.innerHTML = "lalalala";
+	e.target.innerHTML =
+		"Ne jedhe mitra kraune lala lala ohh geraa kolo kerhe hune aww";
 });
-
+let a = 5;
+a = "Gagnan";
+a = "Ga\23ga";
+console.log(a);
+*/
 // document.getElementById("h").innerHTML = "MY name is Gaganpreet singh";
+
+// pop ups
+
+// 1. alert()
+
+alert("There is a bus behind you.");
+let name1 = prompt("Give me your name ?");
+const con = confirm("Are you sure to see your name in the document ? ");
+if (con === true) {
+	document.write(`your name is : ${name1}`);
+} else {
+	document.write("You are not willing to see your name.");
+}
