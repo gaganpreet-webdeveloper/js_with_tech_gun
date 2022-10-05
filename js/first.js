ac73d0c31a594a1cd496116686f6b734c15aa9b5
gaganpreet-webdeveloper <gagandhani179@gmail.com>
2022-10-05 16:06:56 +0530

    update

--- js/first.js
+++ js/first.js
@@ -15,11 +15,39 @@ console.log(a);
 
 // 1. alert()
 
-alert("There is a bus behind you.");
-let name1 = prompt("Give me your name ?");
-const con = confirm("Are you sure to see your name in the document ? ");
-if (con === true) {
-	document.write(`your name is : ${name1}`);
-} else {
-	document.write("You are not willing to see your name.");
+// alert("There is a bus behind you.");
+// let name1 = prompt("Give me your name ?");
+// const con = confirm("Are you sure to see your name in the document ? ");
+// if (con === true) {
+// 	document.write(`your name is : ${name1}`);
+// } else {
+// 	document.write("You are not willing to see your name.");
+// }
+
+// THE ARGUMENT object (By using this object we create a generalize add function)
+
+// function add() {
+// 	let res = 0;
+// 	for (let i = 0; i < arguments.length; i++) {
+// 		res += arguments[i];
+// 	}
+// 	console.log("The addition result value is : " + res);
+// }
+
+// add(3, 5);
+
+// Object in js
+
+const person = {
+	//properties
+	name: "Gaganpreet Singh",
+	class: "MCA",
+	//Methods
+	sayhello() {
+		console.log("Gagan is the only king on the earth");
+	},
+};
+console.log(person);
+for (key in person) {
+	console.log(key + " : " + person[key]);
 }
